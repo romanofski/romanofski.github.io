e68b25555c2defc12a853cb25203ed9ab38fd56b
Roman Joost <rjoost@redhat.com>
2015-09-14 19:52:00 +1000

    No need to keep these scripts

--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xe
-make
-git add -A .
-git commit -m "Updated"
-git push origin master --force
--- watchDropbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -xe
-
-filetowatch=~/Dropbox/blog.markdown
-
-while true
-do
-    inotifywait -e close_write $filetowatch && $1
-done
